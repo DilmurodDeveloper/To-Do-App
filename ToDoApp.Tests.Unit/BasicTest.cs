f4099ac11da7b3fdb08d4e6a0e28bd525951b50e
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-05-29 17:18:46 +0500

    INFRA: Initialize Unit Test Project

---
+++ ToDoApp.Tests.Unit/BasicTest.cs
@@ -0,0 +1,11 @@
+namespace ToDoApp.Tests.Unit
+{
+    public class BasicTest
+    {
+        [Fact]
+        public void SampleTest_Pass()
+        {
+            Assert.True(true);
+        }
+    }
+}
